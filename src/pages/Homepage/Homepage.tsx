4033fd1a9d082a41f659f92eaea0d7a8ee4fbfa2
jubayerjuhan <davidjuhan23@gmail.com>
2024-08-28 02:30:47 +0600

    Added Review Section Under The CTA Button

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MobileNavbar, Navbar } from '~components/index';
 
+import StarImage from '~assets/images/star.png';
 import './Homepage.scss';
 
 export const Homepage = () => {
@@ -24,7 +25,7 @@ export const Homepage = () => {
               <span className="highlight">AI-Powered</span> <br />
               Homework Assistance
             </h1>
-            <p>
+            <p className="description">
               Experience swift and dependable homework help with our AI-driven
               solution, ensuring clarity and improved academic results.
             </p>
@@ -32,15 +33,28 @@ export const Homepage = () => {
               Get Started Free
             </a>
             <div className="reviews">
-              <span>15K reviews on</span>
-              <span className="rating">
-                <img src="star-icon.png" alt="Star" /> 4.9
-              </span>
               <div className="review-avatars">
-                <img src="avatar1.png" alt="Reviewer" />
-                <img src="avatar2.png" alt="Reviewer" />
-                <img src="avatar3.png" alt="Reviewer" />
+                <img
+                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724630400&semt=ais_hybrid"
+                  alt="Reviewer"
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724630400&semt=ais_hybrid"
+                  alt="Reviewer"
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724630400&semt=ais_hybrid"
+                  alt="Reviewer"
+                />
+                <img
+                  src="https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1724630400&semt=ais_hybrid"
+                  alt="Reviewer"
+                />
               </div>
+              <span className="rating">
+                <img src={StarImage} alt="Star" />
+                <p>4,000+ Students Boosting Grades with Us</p>
+              </span>
             </div>
           </div>
           <div className="hero-image">
