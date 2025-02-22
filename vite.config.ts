22e865fc94961efb2d7b51206a982f78f54065f3
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-22 14:42:02 +0600

    Fix: removed viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,14 +10,4 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['zod'],
-      output: {
-        globals: {
-          zod: 'Zod',
-        },
-      },
-    },
-  },
 });
