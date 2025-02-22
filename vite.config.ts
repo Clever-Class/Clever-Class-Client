eab440d7d2d2440294e391458f3d2dcdabf5f163
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-22 14:33:48 +0600

    Update: added new features and fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      external: ['zod'],
+      output: {
+        globals: {
+          zod: 'Zod',
+        },
+      },
+    },
+  },
 });
