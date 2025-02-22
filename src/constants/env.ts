f15137d2f03c9fec4c875e019d4f4ec94917fb16
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-22 14:26:02 +0600

    Fix: changed env.ts

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,7 @@
-export const NODE_ENV = 'development';
+export const NODE_ENV = import.meta.env.NODE_ENV as string;
 export const PADDLE_TOKEN = import.meta.env.VITE_PADDLE_TOKEN as string;
-export const PADDLE_ENVIRONMENT = 'sandbox';
+export const PADDLE_ENVIRONMENT = import.meta.env
+  .VITE_PADDLE_ENVIRONMENT as string;
 
 export const DEFAULT_SELECTED_PACKAGE = import.meta.env
   .VITE_DEFAULT_SELECTED_PACKAGE as string;
